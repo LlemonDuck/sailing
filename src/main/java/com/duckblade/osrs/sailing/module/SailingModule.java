1419f1ac33e6a302c4e93e9f1eaeec9271ccbe6f
Rhea <napkinorton@gmail.com>
2025-11-23 16:41:45 -0500

    module: gate CargoHoldTracker behind developerMode

--- src/main/java/com/duckblade/osrs/sailing/module/SailingModule.java
+++ src/main/java/com/duckblade/osrs/sailing/module/SailingModule.java
@@ -1,22 +1,24 @@
 package com.duckblade.osrs.sailing.module;
 
-import com.duckblade.osrs.sailing.features.crewmates.CrewmateOverheadMuter;
+import com.duckblade.osrs.sailing.SailingConfig;
+import com.duckblade.osrs.sailing.features.CargoHoldTracker;
+import com.duckblade.osrs.sailing.features.LuffOverlay;
+import com.duckblade.osrs.sailing.features.RapidsOverlay;
+import com.duckblade.osrs.sailing.features.barracudatrials.BarracudaTrialHelper;
+import com.duckblade.osrs.sailing.features.charting.SeaChartOverlay;
 import com.duckblade.osrs.sailing.features.charting.SeaChartPanelOverlay;
 import com.duckblade.osrs.sailing.features.charting.SeaChartTaskIndex;
 import com.duckblade.osrs.sailing.features.charting.WeatherTaskTracker;
-import com.duckblade.osrs.sailing.features.barracudatrials.BarracudaTrialHelper;
+import com.duckblade.osrs.sailing.features.crewmates.CrewmateOverheadMuter;
 import com.duckblade.osrs.sailing.features.mes.DeprioSailsOffHelm;
 import com.duckblade.osrs.sailing.features.mes.PrioritizeCargoHold;
 import com.duckblade.osrs.sailing.features.util.BoatTracker;
-import com.duckblade.osrs.sailing.features.LuffOverlay;
-import com.duckblade.osrs.sailing.features.RapidsOverlay;
-import com.duckblade.osrs.sailing.SailingConfig;
-import com.duckblade.osrs.sailing.features.charting.SeaChartOverlay;
 import com.google.common.collect.ImmutableSet;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import java.util.Set;
+import javax.inject.Named;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.client.config.ConfigManager;
 
@@ -32,9 +34,11 @@ public class SailingModule extends AbstractModule
 
 	@Provides
 	Set<PluginLifecycleComponent> lifecycleComponents(
+		@Named("developerMode") boolean developerMode,
+
 		BarracudaTrialHelper barracudaTrialHelper,
 		BoatTracker boatTracker,
-//		CargoHoldTracker cargoHoldTracker,
+		CargoHoldTracker cargoHoldTracker,
 		CrewmateOverheadMuter crewmateOverheadMuter,
 		DeprioSailsOffHelm deprioSailsOffHelm,
 		LuffOverlay luffOverlay,
@@ -46,20 +50,28 @@ public class SailingModule extends AbstractModule
 		WeatherTaskTracker weatherTaskTracker
 	)
 	{
-		return ImmutableSet.of(
-			barracudaTrialHelper,
-			boatTracker,
-//			cargoHoldTracker,
-			crewmateOverheadMuter,
-			deprioSailsOffHelm,
-			luffOverlay,
-			prioritizeCargoHold,
-			rapidsOverlay,
-			seaChartOverlay,
-			seaChartPanelOverlay,
-			seaChartTaskIndex,
-			weatherTaskTracker
-		);
+		var builder = ImmutableSet.<PluginLifecycleComponent>builder()
+			.add(barracudaTrialHelper)
+			.add(boatTracker)
+			.add(cargoHoldTracker)
+			.add(crewmateOverheadMuter)
+			.add(deprioSailsOffHelm)
+			.add(luffOverlay)
+			.add(prioritizeCargoHold)
+			.add(rapidsOverlay)
+			.add(seaChartOverlay)
+			.add(seaChartPanelOverlay)
+			.add(seaChartTaskIndex)
+			.add(weatherTaskTracker);
+
+		// features still in development
+		if (developerMode)
+		{
+			builder
+				.add(cargoHoldTracker);
+		}
+
+		return builder.build();
 	}
 
 	@Provides
