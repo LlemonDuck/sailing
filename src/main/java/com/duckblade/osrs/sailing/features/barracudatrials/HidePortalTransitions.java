3e9f3e257647cc67a344ee4b6151db2f6563a576
NathanVegetable <nathan.dabutt@gmail.com>
2025-11-29 13:47:15 -0800

    Fix 136 - Hide Gwenith Glide Portal Transitions

--- src/main/java/com/duckblade/osrs/sailing/features/barracudatrials/HidePortalTransitions.java
+++ src/main/java/com/duckblade/osrs/sailing/features/barracudatrials/HidePortalTransitions.java
@@ -3,13 +3,13 @@ package com.duckblade.osrs.sailing.features.barracudatrials;
 import com.duckblade.osrs.sailing.SailingConfig;
 import com.duckblade.osrs.sailing.features.util.SailingUtil;
 import com.duckblade.osrs.sailing.module.PluginLifecycleComponent;
-import java.util.Arrays;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.api.events.ScriptPreFired;
+import net.runelite.api.gameval.VarbitID;
 import net.runelite.client.eventbus.Subscribe;
 
 @Slf4j
@@ -18,7 +18,8 @@ import net.runelite.client.eventbus.Subscribe;
 public class HidePortalTransitions
 	implements PluginLifecycleComponent
 {
-	private static final int SCRIPT_PORTAL_TRANSITION_EFFECT = 5986;
+	private static final int FADE_TRANSITION_SCRIPT_ID = 948;
+	private boolean isFadingOutDuringBarracudaTrial = false;
 
 	private final Client client;
 
@@ -31,7 +32,7 @@ public class HidePortalTransitions
 	@Subscribe
 	public void onScriptPreFired(ScriptPreFired event)
 	{
-		if (event.getScriptId() != SCRIPT_PORTAL_TRANSITION_EFFECT)
+		if (event.getScriptId() != FADE_TRANSITION_SCRIPT_ID)
 		{
 			return;
 		}
@@ -41,7 +42,36 @@ public class HidePortalTransitions
 			return;
 		}
 
-		Arrays.fill(event.getScriptEvent().getArguments(), 0);
+		var gwenithGlideMasterState = client.getVarbitValue(VarbitID.SAILING_BT_GWENITH_GLIDE_MASTER_STATE);
+
+		var isInGwenithGlideTrial = gwenithGlideMasterState >= 2;
+		if (!isInGwenithGlideTrial)
+		{
+			return;
+		}
+
+		var args = event.getScriptEvent().getArguments();
+		if (args.length < 6)
+		{
+			return;
+		}
+
+		var isFadingOut = (int)args[2] == 255;
+		if (isFadingOut)
+		{
+			isFadingOutDuringBarracudaTrial = true;
+			args[4] = 255;
+			args[5] = 0;
+			return;
+		}
+
+		var isFadingIn = (int)args[4] == 255;
+		if (isFadingOutDuringBarracudaTrial && isFadingIn)
+		{
+			isFadingOutDuringBarracudaTrial = false;
+			args[2] = 255;
+			args[5] = 0;
+		}
 	}
 
 }
